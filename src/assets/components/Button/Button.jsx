b05aa8f6a5bd309a2dafd88c45864335eecf1f1c
DFStechBox <dfosterstewart@gmail.com>
2024-02-04 20:19:45 -0800

    many changes/updates

--- src/assets/components/Button/Button.jsx
+++ src/assets/components/Button/Button.jsx
@@ -1,9 +1,15 @@
 import styles from "./Button.module.css";
+import PropTypes from "prop-types";
 
 function Button({ className, children }) {
   return (
     <button className={`${className} ${styles.button}`}>{children}</button>
   );
 }
+
+Button.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
 
 export default Button;
